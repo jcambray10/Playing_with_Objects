75a5c64a99936ce9b54795ecc42f8799148110fa
jcambray10 <84952483+jcambray10@users.noreply.github.com>
2021-07-25 22:20:42 -0500

    first commit

---
+++ Playing_with_Objects.js
@@ -0,0 +1,31 @@
+var users = [
+        {
+            name: "Michael",
+            age: 37
+        },
+        {
+            name: "John",
+            age: 30
+        },
+        {
+            name: "David",
+            age: 27
+        }
+    ];
+    console.log(users[0].name);
+    // console.log(users(,30))
+
+// How would you Pring/log John's age?
+console.log(users[1].age);
+
+// How would you print/log the name of the first object?
+console.log(users[0].name);
+// How would you print/log the name and age of each user using a for loop?  Your output should look something like this
+
+// echo "# Playing_with_Objects" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/jcambray10/Playing_with_Objects.git
+// git push -u origin main
